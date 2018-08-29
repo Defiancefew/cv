ad1d47c48390a905c0c923a1f35d0a764ef6da0e
Defiancefew <Severancepwns@gmail.com>
2018-08-29 13:23:16 +0300

    cleaned layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,8 +4,6 @@ export const Layout = styled.div`
   max-width: 1280px;
   display: flex;
   flex-direction: row;
-  /* border-left: 1px solid gray; */
-  /* border-right: 1px solid gray; */
   min-height: 100%;
-  margin: 0 auto;
+  margin: 0 auto 100px auto;
 `;
